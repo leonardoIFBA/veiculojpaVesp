7f3316265d063942bc234dd7ab4a7f7be8d3ef9d
leonardoIFBA <leonardorodrigues@ifba.edu.br>
2024-07-24 10:47:34 -0300

    Atualizando repositorio.

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import com.example.model.Veiculo;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Veiculo v = new Veiculo();
+        v.setCodigo(4L);
+        v.setFabricante("BMW");
+        v.setModelo("X2");
+        v.setAnoFabricacao(2024);
+        v.setAnoModelo(2024);
+        v.setValor(250000D);
+
+        EntityManagerFactory entityManagerFactory = Persistence.
+            createEntityManagerFactory("IFBA-PU");
+
+        EntityManager manager = entityManagerFactory.createEntityManager();
+        EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+        manager.persist(v);
+        tx.commit();
+        
+        entityManagerFactory.close();
+    }
+}
